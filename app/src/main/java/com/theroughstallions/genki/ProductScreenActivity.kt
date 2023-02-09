10bc70ba18c3be95cdac33337caa1838434d9f4c
Marcus Laguerre <110794840+markraft@users.noreply.github.com>
2023-02-09 18:34:55 -0500

    Added back button for action bar

--- app/src/main/java/com/theroughstallions/genki/ProductScreenActivity.kt
+++ app/src/main/java/com/theroughstallions/genki/ProductScreenActivity.kt
@@ -8,5 +8,23 @@ class ProductScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_screen)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
