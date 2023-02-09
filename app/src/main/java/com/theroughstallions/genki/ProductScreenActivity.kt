62e115acc68123a670e44fa1c0544710d196f72b
Marcus Laguerre <110794840+markraft@users.noreply.github.com>
2023-02-09 18:34:19 -0500

    Added product screen

---
+++ app/src/main/java/com/theroughstallions/genki/ProductScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.theroughstallions.genki
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class ProductScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_screen)
+    }
+}
